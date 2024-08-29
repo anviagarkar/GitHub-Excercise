9299ed615ab7dcfb4c4c00d95ce5f2c685f37193
anviagarkar <139617645+anviagarkar@users.noreply.github.com>
2024-08-29 17:19:37 -0400

    Initial Commit

---
+++ GitHub Excercise/GitHub_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExcerciseApp.swift
+//  GitHub Excercise
+//
+//  Created by Anvi Agarkar on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
